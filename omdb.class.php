92c5a845b9f24d7368b42a47eca9f258ff5ebe8e
Wansumkok <carl.j.zhou@gmail.com>
2014-02-22 19:42:00 -0500

    fixed bug that caused 400 errors

--- omdb.class.php
+++ omdb.class.php
@@ -10,6 +10,7 @@ class omdb{
 		curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		curl_setopt($ch, CURLOPT_HEADER, false);
 		$response = curl_exec($ch);
 		
@@ -21,8 +22,7 @@ class omdb{
 			}
 		$data=json_decode($response);
 		if($data->Response=="False"){
-			echo $data->Error . "<br>\n";
-			return false;
+			die("Error: " . $data->Error . "<br>\n");
 		}
 		else{
 			return $data;
@@ -34,7 +34,17 @@ class omdb{
 		$year="y=" . $params['year'];
 		$imdbid="i=" . $params['imdbid'];
 		$title="t=" . $params['title'];
+		$title=str_replace(" ","+",$title); //necessary, otherwise the search will result in a 400 error response
 		$url=self::apiurl . $year . "&" . $imdbid . "&" . $title;
+		$data=self::getdata($url);
+		return $data;
+	}
+	
+	//$term must be nonempty otherwise no results will be returned
+	static function search($term){
+		$term=str_replace(" ","+",$term); //necessary, otherwise the search will result in a 400 error response
+		$q="s=" . $term;
+		$url=self::apiurl . $q;
 		$data=self::getdata($url);
 		return $data;
 	}
