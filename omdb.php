64c4a9d52b559dba0c6f9e5651e31ed21d02de0f
Wansumkok <carl.j.zhou@gmail.com>
2014-02-22 19:42:50 -0500

    included new class

--- omdb.php
+++ omdb.php
@@ -1,11 +1,17 @@
 <?php
 /*PHP class for OMDB
 Author: Carl Zhou
-Version:1
+Version:1.01
+
+Changes in 1.01:
+-fixed bug that caused 400 errors
+-added function for searching movies
+-minor code changes
 
 OMDB API by Brian Fritz
 */
 
 require_once("omdb.class.php");
 require_once("omdb_movie.class.php");
+require_once("omdb_search.class.php");
 ?>
