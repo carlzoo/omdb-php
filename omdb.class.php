ae1d3e4be6c953d1fb9eeb46b01c4b393e1e3338
Wansumkok <carl.j.zhou@gmail.com>
2014-02-26 19:31:06 -0500

    fixed a glaring error in curlopt_headers

--- omdb.class.php
+++ omdb.class.php
@@ -7,7 +7,7 @@ class omdb{
 	//returns false if no movie is found, otherwise returns json decoded data
 	static function getdata($url){
 		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_HEADER, 1);
+		//curl_setopt($ch, CURLOPT_HEADER, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
